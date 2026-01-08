5dd434a3fdcc19e8629ea111719b553c2a03963f
René Omar Acosta <85846595+Rakosta@users.noreply.github.com>
2026-01-08 01:54:27 -0600

    Update firmware_chaleco.ino

--- src/iot/chalecos/firmware_chaleco.ino
+++ src/iot/chalecos/firmware_chaleco.ino
@@ -1,1 +1,44 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
 
+const char* ssid = "TU_WIFI";
+const char* password = "TU_PASSWORD";
+
+const char* mqtt_server = "test.mosquitto.org";
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    if (client.connect("ChalecoInteligente")) {
+      client.subscribe("agi-system/alertas");
+    } else {
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  // Simulación de signos vitales
+  String payload = "{\"trabajador_id\":2, \"tipo\":\"signos\", \"signos_vitales\":{\"pulso\":120}}";
+  client.publish("agi-system/alertas", payload.c_str());
+  delay(5000);
+}
